3f6bd7275ae125060600ca1691ac9a626afaae57
Luan Santos <git@luan.sh>
2023-07-03 14:03:31 -0700

    feat: config

--- main.go
+++ main.go
@@ -2,64 +2,136 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
-var newLineByte = []byte{0x0a}
+var (
+	properties = []string{
+		"loginWebService",
+		"clientWebService",
+		"tibiaPageUrl",
+		"tibiaStoreGetCoinsUrl",
+		"getPremiumUrl",
+		"createAccountUrl",
+		"accessAccountUrl",
+		"lostAccountUrl",
+		"manualUrl",
+		"faqUrl",
+		"premiumFeaturesUrl",
+		"crashReportUrl",
+		"fpsHistoryRecipient",
+		"cipSoftUrl",
+	}
+)
+
 var paddingByte = []byte{0x20}
 var battleyeHex = []byte{0x8d, 0x4d, 0x80, 0x75, 0x0e, 0xe8, 0x2e, 0xc8}
 var removeBattleyeHex = []byte{0x8d, 0x4d, 0x80, 0xeb, 0x0e, 0xe8, 0x2e, 0xc8}
 
-const propertyLoginWebService = "loginWebService="
-const propertyClientWebService = "clientWebService="
-
 func main() {
-	var currentExecutable, tibiaExe, customLoginWebService string
-	var err error
+	var configFile, tibiaExe string
+	flag.StringVar(&configFile, "c", "config.toml", "Path to the config file")
+	flag.StringVar(&tibiaExe, "t", getDefaultTibiaExe(), "Path to Tibia executable")
 
-	args := os.Args
-	if len(args) > 0 {
-		currentExecutable = args[0]
-	}
+	flag.Parse()
 
-	if len(args) > 1 {
-		tibiaExe = args[1]
-	}
+	currentExecutable := os.Args[0]
+	viper.SetConfigFile(configFile)
 
-	if len(args) > 2 {
-		customLoginWebService = args[2]
-	}
-
-	if currentExecutable == "" || tibiaExe == "" || customLoginWebService == "" {
-		fmt.Printf("USAGE: %s <Tibia.exe Path> <Address Login Service>\n", currentExecutable)
+	if currentExecutable == "" || tibiaExe == "" {
+		printUsage(currentExecutable)
 		os.Exit(1)
 	}
 
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to read config file: %s\n", err.Error())
+		os.Exit(1)
+	}
+	// Check if all properties are present in the config file
+	missingProperties := make([]string, 0)
+	for _, prop := range properties {
+		if !viper.IsSet(prop) {
+			missingProperties = append(missingProperties, prop)
+		}
+	}
+
+	// Error out if any properties are missing
+	if len(missingProperties) > 0 {
+		fmt.Printf("[ERROR] Missing properties in the config file: %v\n", missingProperties)
+		os.Exit(1)
+	}
+
+	configValues := make(map[string]string)
+	for _, prop := range properties {
+		value := viper.GetString(prop)
+		configValues[prop] = value
+	}
+
 	tibiaPath, tibiaBinary := readFile(tibiaExe)
-	var originalBinarySize = len(tibiaBinary)
+	originalBinarySize := len(tibiaBinary)
 
-	_, tibiaRsa := readFile("tibia_rsa.key")
-	_, otservRsa := readFile("otserv_rsa.key")
+	backupTibiaExecutable(tibiaPath, tibiaBinary)
 
+	tibiaBinary = replaceTibiaRSAKey(tibiaBinary)
+	tibiaBinary = removeBattlEye(tibiaBinary)
+
+	for prop, value := range configValues {
+		ok := setPropertyByName(tibiaBinary, prop, value)
+		if !ok {
+			fmt.Printf("[ERROR] Unable to replace %s\n", prop)
+			os.Exit(1)
+		}
+	}
+
+	exportModifiedFile(tibiaPath, tibiaBinary, originalBinarySize)
+}
+
+func getDefaultTibiaExe() string {
+	if runtime.GOOS == "windows" {
+		return "client.exe"
+	}
+	return "client"
+}
+
+func printUsage(currentExecutable string) {
+	fmt.Printf("USAGE: %s -t <Tibia.exe Path> -c <Config file>\n", currentExecutable)
+}
+
+func backupTibiaExecutable(tibiaPath string, tibiaBinary []byte) {
 	tibiaExeFileName := filepath.Base(tibiaPath)
 	tibiaExeBackupPath := filepath.Join(filepath.Dir(tibiaPath), fmt.Sprintf("BKP%d-%s", time.Now().Unix(), tibiaExeFileName))
 	tibiaExeBackupFileName := filepath.Base(tibiaExeBackupPath)
 
-	fmt.Printf("[INFO] Backuping %s to %s\n", tibiaExeFileName, tibiaExeBackupFileName)
-	err = os.WriteFile(tibiaExeBackupPath, tibiaBinary, 0644)
+	fmt.Printf("[INFO] Backing up %s to %s\n", tibiaExeFileName, tibiaExeBackupFileName)
+
+	err := os.WriteFile(tibiaExeBackupPath, tibiaBinary, 0644)
 	if err != nil {
 		fmt.Printf("[ERROR] %s\n", err.Error())
 		os.Exit(1)
 	}
+}
+
+func replaceTibiaRSAKey(tibiaBinary []byte) []byte {
+	tibiaRsaPath := "tibia_rsa.key"
+	otservRsaPath := "otserv_rsa.key"
+
+	_, tibiaRsa := readFile(tibiaRsaPath)
+	_, otservRsa := readFile(otservRsaPath)
 
 	fmt.Printf("[INFO] Searching for Tibia RSA... \n")
+
 	if bytes.ContainsAny(tibiaBinary, string(tibiaRsa)) {
 		fmt.Printf("[INFO] Tibia RSA found!\n")
 		tibiaBinary = bytes.Replace(tibiaBinary, tibiaRsa, otservRsa, 1)
-		fmt.Printf("[PATCH] Tibia RSA replaced to OTServ RSA!\n")
+		fmt.Printf("[PATCH] Tibia RSA replaced with OTServ RSA!\n")
 	} else if bytes.ContainsAny(tibiaBinary, string(otservRsa)) {
 		fmt.Printf("[WARN] OTServ RSA already patched!\n")
 	} else {
@@ -67,7 +139,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	return tibiaBinary
+}
+
+func removeBattlEye(tibiaBinary []byte) []byte {
 	fmt.Printf("[INFO] Searching for Battleye... \n")
+
 	if bytes.Contains(tibiaBinary, battleyeHex) {
 		fmt.Printf("[INFO] Battleye found!\n")
 		tibiaBinary = bytes.Replace(tibiaBinary, battleyeHex, removeBattleyeHex, 1)
@@ -75,116 +152,75 @@ func main() {
 	} else if bytes.Contains(tibiaBinary, removeBattleyeHex) {
 		fmt.Printf("[WARN] Battleye already removed!\n")
 	} else {
-		fmt.Printf("[ERROR] Unable to find Battleye\n")
+		fmt.Printf("[WARN] Battleye not found\n")
 	}
 
-	fmt.Printf("[INFO] Searching for Login WebService... \n")
-	var replaced bool
-
-	// Find in the binary where we have these properties
-	var loginPropertyIndex = bytes.Index(tibiaBinary, []byte(propertyLoginWebService))
-	var clientPropertyIndex = bytes.Index(tibiaBinary, []byte(propertyClientWebService))
-
-	if loginPropertyIndex != -1 {
-		// Extract current login web service
-		var startLoginWebServiceValue = loginPropertyIndex + len(propertyLoginWebService)
-		var endLoginWebServiceValue = startLoginWebServiceValue + bytes.Index(tibiaBinary[startLoginWebServiceValue:], newLineByte)
-		var loginWebServiceValue = string(tibiaBinary[startLoginWebServiceValue:endLoginWebServiceValue])
-
-		if len(customLoginWebService) > len(loginWebServiceValue) {
-			fmt.Printf("[ERROR] Cannot replace webservice to %s, because the new loginWebService must be smaller than '%s' (%d chars).\n", customLoginWebService, loginWebServiceValue, len(loginWebServiceValue))
-			os.Exit(1)
-		}
-
-		fmt.Printf("[INFO] Tibia Login WebService found! %s\n", loginWebServiceValue)
-
-		// Create the new services with the correct length
-		var customWebService = []byte(customLoginWebService)
-		var paddedCustomLoginWebService = append(customWebService, bytes.Repeat(paddingByte, len(loginWebServiceValue)-len(customLoginWebService))...)
-
-		// Merge everything back to the client
-		remainingOfBinary := tibiaBinary[endLoginWebServiceValue:]
-
-		tibiaBinary = append(tibiaBinary[:startLoginWebServiceValue], paddedCustomLoginWebService...)
-		tibiaBinary = append(tibiaBinary, remainingOfBinary...)
-
-		if originalBinarySize != len(tibiaBinary) {
-			fmt.Printf("[ERROR] Fatal error: The new modified client (size %d) has different bytesize from the original (size %d). Make sure to use the correct versions of both the client and client-editor or report a bug\n", len(tibiaBinary), originalBinarySize)
-			os.Exit(1)
-		}
-
-		fmt.Printf("[PATCH] Tibia Login WebService replaced to %s!\n", customLoginWebService)
-		replaced = true
-	} else {
-		fmt.Printf("[WARNING] Tibia Login WebService was not found! \n")
-	}
-
-	if clientPropertyIndex != -1 {
-		// Extract current client web service
-		var startClientWebServiceValue = clientPropertyIndex + len(propertyClientWebService)
-		var endClientWebServiceValue = startClientWebServiceValue + bytes.Index(tibiaBinary[startClientWebServiceValue:], newLineByte)
-		var clientWebServiceValue = string(tibiaBinary[startClientWebServiceValue:endClientWebServiceValue])
-
-		if len(customLoginWebService) > len(clientWebServiceValue) {
-			fmt.Printf("[ERROR] Cannot replace webservice to %s, because the new clientWebService must be smaller than '%s' (%d chars).\n", customLoginWebService, clientWebServiceValue, len(clientWebServiceValue))
-			os.Exit(1)
-		}
-
-		fmt.Printf("[INFO] Tibia Client WebService found! %s\n", clientWebServiceValue)
-
-		// Create the new services with the correct length
-		var customWebService = []byte(customLoginWebService)
-		var paddedCustomClientWebService = append(customWebService, bytes.Repeat(paddingByte, len(clientWebServiceValue)-len(customLoginWebService))...)
-
-		// Merge everything back to the client
-		remainingOfBinary := tibiaBinary[endClientWebServiceValue:]
-
-		tibiaBinary = append(tibiaBinary[:startClientWebServiceValue], paddedCustomClientWebService...)
-		tibiaBinary = append(tibiaBinary, remainingOfBinary...)
-
-		if originalBinarySize != len(tibiaBinary) {
-			fmt.Printf("[ERROR] Fatal error: The new modified client (size %d) has different bytesize from the original (size %d). Make sure to use the correct versions of both the client and client-editor or report a bug\n", len(tibiaBinary), originalBinarySize)
-			os.Exit(1)
-		}
-
-		fmt.Printf("[PATCH] Tibia Client WebService replaced to %s!\n", customLoginWebService)
-		replaced = true
-	} else {
-		fmt.Printf("[WARNING] Tibia Client WebService was not found! Your client version might not require it. \n")
-	}
-
-	if !replaced {
-		fmt.Printf("[ERROR] Unable to replace Tibia Login or Client WebService\n")
-		os.Exit(1)
-	}
-
-	fmt.Printf("[PATCH] Exporting File!\n")
-	err = os.WriteFile(tibiaPath, tibiaBinary, 0644)
-	if err != nil {
-		fmt.Printf("[ERROR] %s\n", err.Error())
-		os.Exit(1)
-	}
+	return tibiaBinary
 }
 
-func readFile(path string) (string, []byte) {
-	fileAbs, err := filepath.Abs(path)
+func exportModifiedFile(tibiaPath string, tibiaBinary []byte, originalBinarySize int) {
+	outputFilePath := tibiaPath
+
+	if len(tibiaBinary) != originalBinarySize {
+		fmt.Printf("[ERROR] Invalid patched file size, original: %d, modified: %d\n", originalBinarySize, len(tibiaBinary))
+		os.Exit(1)
+	}
+
+	err := os.WriteFile(outputFilePath, tibiaBinary, 0644)
 	if err != nil {
 		fmt.Printf("[ERROR] %s\n", err.Error())
 		os.Exit(1)
 	}
 
-	fileName := filepath.Base(path)
+	fmt.Printf("[INFO] Patched file exported to: %s\n", outputFilePath)
+}
 
-	if _, err := os.Stat(fileAbs); os.IsNotExist(err) {
-		fmt.Printf("[ERROR] cannot locate %s at %s\n", fileName, path)
-		os.Exit(1)
-	}
-
-	raw, err := os.ReadFile(fileAbs)
+func readFile(filePath string) (string, []byte) {
+	fileData, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Printf("[ERROR] %s\n", err.Error())
 		os.Exit(1)
 	}
 
-	return fileAbs, raw
+	return filePath, fileData
+}
+
+func setPropertyByName(tibiaBinary []byte, propertyName string, customValue string) bool {
+	originalBinarySize := len(tibiaBinary)
+	propertyName = fmt.Sprintf("%s=", propertyName)
+	propertyIndex := bytes.Index(tibiaBinary, []byte(propertyName))
+	if propertyIndex != -1 {
+		// Extract current property value
+		startValue := propertyIndex + len(propertyName)
+		endValue := startValue + bytes.IndexByte(tibiaBinary[startValue:], '\n')
+		propertyValue := string(tibiaBinary[startValue:endValue])
+
+		if len(customValue) > len(propertyValue) {
+			fmt.Printf("[ERROR] Cannot replace %s to '%s' because the new value must be smaller than '%s' (%d chars).\n", propertyName, customValue, propertyValue, len(propertyValue))
+			os.Exit(1)
+		}
+
+		fmt.Printf("[INFO] %s found! %s\n", propertyName, propertyValue)
+
+		// Create the new value with the correct length
+		customValueBytes := []byte(customValue)
+		paddedCustomValue := append(customValueBytes, bytes.Repeat(paddingByte, len(propertyValue)-len(customValueBytes))...)
+
+		// Merge everything back to the client
+		remainingBinary := tibiaBinary[endValue:]
+
+		tibiaBinary = append(tibiaBinary[:startValue], paddedCustomValue...)
+		tibiaBinary = append(tibiaBinary, remainingBinary...)
+
+		if originalBinarySize != len(tibiaBinary) {
+			fmt.Printf("[ERROR] Fatal error: The new modified client (size %d) has a different byte size from the original (size %d). Make sure to use the correct versions of both the client and client-editor or report a bug.\n", len(tibiaBinary), originalBinarySize)
+			os.Exit(1)
+		}
+
+		fmt.Printf("[PATCH] %s replaced to %s!\n", propertyName, customValue)
+		return true
+	}
+
+	fmt.Printf("[WARNING] %s was not found!\n", propertyName)
+	return false
 }

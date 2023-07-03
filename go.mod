42b5e8559a6aa71042812d90ff4db05176c09fe8
Luan Santos <git@luan.sh>
2023-07-03 14:03:31 -0700

    feat: repack

--- go.mod
+++ go.mod
@@ -7,8 +7,12 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/schollz/progressbar/v3 v3.13.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
@@ -17,7 +21,8 @@ require (
 	github.com/spf13/viper v1.16.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/ulikunitz/xz v0.5.11 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/term v0.9.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ repack/repack.go
@@ -0,0 +1,246 @@
+package repack
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/schollz/progressbar/v3"
+	"github.com/ulikunitz/xz/lzma"
+)
+
+type File struct {
+	LocalFile    string `json:"localfile"`
+	PackedHash   string `json:"packedhash"`
+	PackedSize   int64  `json:"packedsize"`
+	URL          string `json:"url"`
+	UnpackedHash string `json:"unpackedhash"`
+	UnpackedSize int64  `json:"unpackedsize"`
+}
+
+type AssetsInfo struct {
+	Files   []File `json:"files"`
+	Version int    `json:"version"`
+}
+
+type ClientInfo struct {
+	Version    string `json:"version"`
+	Files      []File `json:"files"`
+	Executable string `json:"executable"`
+	Generation string `json:"generation"`
+	Variant    string `json:"variant"`
+	Revision   int    `json:"revision"`
+}
+
+func repackFiles(src, dst string) error {
+	clientFilePath := filepath.Join(src, "client.json")
+	assetsFilePath := filepath.Join(src, "assets.json")
+
+	// Read the existing client info
+	clientInfo, err := readClientInfo(clientFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to read client info: %w", err)
+	}
+
+	// Read the existing assets info
+	assetsInfo, err := readAssetsInfo(assetsFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to read assets info: %w", err)
+	}
+
+	// Create a new directory for repacked files
+	err = os.MkdirAll(dst, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create repacked directory: %w", err)
+	}
+
+	fmt.Printf("Repacking %d client files and %d assets files\n", len(clientInfo.Files), len(assetsInfo.Files))
+
+	bar := progressbar.Default(int64(len(clientInfo.Files) + len(assetsInfo.Files)))
+
+	for i := range clientInfo.Files {
+		err := repackFile(&clientInfo.Files[i], src, dst)
+		if err != nil {
+			return fmt.Errorf("failed to repack file: %w", err)
+		}
+		bar.Add(1)
+	}
+
+	for i := range assetsInfo.Files {
+		err := repackFile(&assetsInfo.Files[i], src, dst)
+		if err != nil {
+			return fmt.Errorf("failed to repack file: %w", err)
+		}
+		bar.Add(1)
+	}
+
+	for i := len(clientInfo.Files) - 1; i >= 0; i-- {
+		if clientInfo.Files[i].UnpackedSize == 0 {
+			clientInfo.Files = append(clientInfo.Files[:i], clientInfo.Files[i+1:]...)
+		}
+	}
+
+	for i := len(assetsInfo.Files) - 1; i >= 0; i-- {
+		if assetsInfo.Files[i].UnpackedSize == 0 {
+			assetsInfo.Files = append(assetsInfo.Files[:i], assetsInfo.Files[i+1:]...)
+		}
+	}
+
+	fmt.Printf("Repacked %d client files and %d assets files\n", len(clientInfo.Files), len(assetsInfo.Files))
+
+	clientInfo.Revision++
+
+	err = saveClientInfo(filepath.Join(dst, "client.json"), clientInfo)
+	if err != nil {
+		return fmt.Errorf("failed to save updated client info: %w", err)
+	}
+	err = saveAssetsInfo(filepath.Join(dst, "assets.json"), assetsInfo)
+	if err != nil {
+		return fmt.Errorf("failed to save updated assets info: %w", err)
+	}
+
+	return nil
+}
+
+func readClientInfo(filePath string) (*ClientInfo, error) {
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var clientInfo ClientInfo
+	err = json.Unmarshal(file, &clientInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &clientInfo, nil
+}
+
+func readAssetsInfo(filePath string) (*AssetsInfo, error) {
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var assetsInfo AssetsInfo
+	err = json.Unmarshal(file, &assetsInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &assetsInfo, nil
+}
+
+func repackFile(file *File, src, dst string) error {
+	localFilePath := filepath.Join(src, file.LocalFile)
+	packedFilePath := filepath.Join(dst, file.URL)
+	if _, err := os.Stat(localFilePath); errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+
+	err := os.MkdirAll(filepath.Dir(packedFilePath), 0755)
+	if err != nil {
+		return err
+	}
+
+	localFile, err := os.Open(localFilePath)
+	if err != nil {
+		return err
+	}
+	defer localFile.Close()
+
+	packedFile, err := os.Create(packedFilePath)
+	if err != nil {
+		return err
+	}
+	defer packedFile.Close()
+
+	if filepath.Ext(packedFilePath) == filepath.Ext(localFilePath) {
+		_, err = io.Copy(packedFile, localFile)
+		if err != nil {
+			return err
+		}
+	} else {
+		lzmaWriter, err := lzma.NewWriter(packedFile)
+		if err != nil {
+			return err
+		}
+		defer lzmaWriter.Close()
+
+		_, err = io.Copy(lzmaWriter, localFile)
+		if err != nil {
+			return err
+		}
+		lzmaWriter.Close()
+	}
+
+	unpackedHash, unpackedSize, err := calculateHashAndSize(localFilePath)
+	if err != nil {
+		return err
+	}
+
+	packedHash, packedSize, err := calculateHashAndSize(packedFilePath)
+	if err != nil {
+		return err
+	}
+
+	file.PackedHash = packedHash
+	file.PackedSize = packedSize
+	file.UnpackedHash = unpackedHash
+	file.UnpackedSize = unpackedSize
+
+	return nil
+}
+
+func calculateHashAndSize(filePath string) (string, int64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", 0, err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", 0, err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return "", 0, err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), fileInfo.Size(), nil
+}
+
+func saveClientInfo(filePath string, clientInfo *ClientInfo) error {
+	clientJSON, err := json.MarshalIndent(clientInfo, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filePath, clientJSON, 0644)
+}
+
+func saveAssetsInfo(filePath string, assetsInfo *AssetsInfo) error {
+	assetsJSON, err := json.MarshalIndent(assetsInfo, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filePath, assetsJSON, 0644)
+}
+
+func Repack(src, dst string) error {
+	fmt.Printf("Repacking %s into %s\n", src, dst)
+	err := repackFiles(src, dst)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+	return nil
+}
